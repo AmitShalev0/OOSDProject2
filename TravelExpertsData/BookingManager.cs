0ee69ec82bf99a232fb9bd2c631f5fbeb9ba6cb0
Azin Ghaffari <azin.ghaffari@gmail.com>
2024-02-14 12:36:10 -0700

    Booking controller

--- TravelExpertsData/BookingManager.cs
+++ TravelExpertsData/BookingManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TravelExpertsData.Migrations;
 
 namespace TravelExpertsData
 {
@@ -18,6 +19,31 @@ namespace TravelExpertsData
             booking.TripTypeId = tripType;
             db.Bookings.Add(booking);
             db.SaveChanges();
+        }
+
+        public static List<BookingDTO> GetBookingsByCustomer(TravelExpertsContext db, int? customerId)
+        {
+            List<BookingDTO> bookings;
+            bookings = (from b in db.Bookings
+                        join p in db.Packages
+                        on b.PackageId equals p.PackageId
+                        join c in db.Customers
+                        on b.CustomerId equals c.CustomerId
+                        where c.CustomerId == customerId
+                        select new BookingDTO
+                        {
+                            BookingId = b.BookingId,
+                            BookingDate = b.BookingDate,
+                            PkgName = p.PkgName,
+                            PkgStartDate = p.PkgStartDate,
+                            PkgEndDate = p.PkgEndDate,
+                            PkgDesc = p.PkgDesc,
+                            PkgBasePrice = p.PkgBasePrice,
+                            PkgAgencyCommission = p.PkgAgencyCommission,
+                            TravelerCount = b.TravelerCount
+                        }).ToList();
+
+            return bookings;
         }
     }
 }
---
+++ TravelExpertsData/Migrations/BookingDTO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExpertsData.Migrations
+{
+    public class BookingDTO
+    {
+        [Display(Name = "Booking ID")]
+        public int BookingId { get; set; }
+
+        [Column(TypeName = "datetime")]
+        [Display(Name = "Booking Date")]
+        public DateTime? BookingDate { get; set; }
+
+        [Display(Name = "Package Name")]
+        public string PkgName { get; set; } = null!;
+
+        [Display(Name = "Start Date")]
+        [Column(TypeName = "datetime")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime? PkgStartDate { get; set; }
+
+        [Column(TypeName = "datetime")]
+        [Display(Name = "End Date")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime? PkgEndDate { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Description")]
+        public string? PkgDesc { get; set; }
+
+        [Column(TypeName = "money")]
+        [Display(Name = "Base Price")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal PkgBasePrice { get; set; }
+
+        [Column(TypeName = "money")]
+        [Display(Name = "Agency Commission")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal? PkgAgencyCommission { get; set; }
+
+        [Display(Name = "Travellers Count")]
+        public double? TravelerCount { get; set; }
+    }
+}

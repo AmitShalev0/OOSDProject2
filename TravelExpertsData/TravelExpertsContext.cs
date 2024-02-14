13f9d8ccdd73c6d79e13cfba68d7eeb8c85e20ae
Amit <110861563+AmitShalev0@users.noreply.github.com>
2024-02-14 12:54:44 -0700

    just sqlexpress from sqlexpress01

--- TravelExpertsData/TravelExpertsContext.cs
+++ TravelExpertsData/TravelExpertsContext.cs
@@ -57,7 +57,7 @@ public partial class TravelExpertsContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseSqlServer(
-            "Data Source=localhost\\sqlexpress01;Initial Catalog=TravelExperts;Integrated Security=True;" +
+            "Data Source=localhost\\sqlexpress;Initial Catalog=TravelExperts;Integrated Security=True;" +
             " TrustServerCertificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
